bc28c99e24c66b12e2e54623835adecd8e5f2465
Daniel Cooke <danielcooke1996@gmail.com>
2023-10-24 15:25:17 +0200

    Performance - cache axios response via nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import { getAllCassettes } from "@/lib/api";
 import { CassetteCard } from "./components/cassette-card";
 import clsx from "clsx";
+import { cache } from "react";
 
+// We are using axios, which is not natively cached
+// by Nextjs - so we have to use cache from react
+// https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#example
+const getCassettes = cache(async () => {
+  return getAllCassettes();
+});
+
+export const revalidate = 3600;
 export default async function Home() {
-  const cassetes = await getAllCassettes();
+  const cassetes = await getCassettes();
   return (
     <main>
       <h1>Audio cassetes</h1>
