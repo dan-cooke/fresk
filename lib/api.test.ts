fad6b3dc17dce82d8fc0ae72c3fb4b661408f74e
Daniel Cooke <danielcooke1996@gmail.com>
2023-10-25 13:38:54 +0200

    Add tests for filtering requirementts

--- lib/api.test.ts
+++ lib/api.test.ts
@@ -28,9 +28,24 @@ describe("API", () => {
             thumb:
               "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
           },
-          { playingTime: "090 minutes" },
-          { type: "Chrome" },
-          { color: "Grey" },
+          { playingTime: "120 minutes" },
+          { type: "Hello" },
+          { color: "Chrome" },
+          { brand: "Apple" },
+        ],
+      },
+
+      {
+        ae9b616f81: [
+          { page: "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.php" },
+          { img: "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.jpg" },
+          {
+            thumb:
+              "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
+          },
+          { playingTime: "60 minutes" },
+          { type: "World" },
+          { color: "Yellow" },
           { brand: "Apple" },
         ],
       },
@@ -45,38 +60,83 @@ describe("API", () => {
 
       it("should map the API response to a consumable format", async () => {
         const cassettes = await getAllCassettes();
-        const expectedResponse: Cassette[] = [
-          {
-            id: "ae9b616f82",
-            page: "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.php",
-            img: "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.jpg",
-            thumb:
-              "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
-            playingTime: "090 minutes",
-            type: "Chrome",
-            color: "Grey",
-            brand: "Apple",
-          },
-          {
-            id: "ae9b616f8d",
-            page: "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.php",
-            img: "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.jpg",
-            thumb:
-              "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
-            playingTime: "090 minutes",
-            type: "Chrome",
-            color: "Grey",
-            brand: "Sony",
-          },
-        ];
 
-        expect(cassettes).toEqual(expectedResponse);
+        expect(cassettes).toMatchInlineSnapshot(`
+[
+  {
+    "brand": "Apple",
+    "color": "Chrome",
+    "id": "ae9b616f82",
+    "img": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.jpg",
+    "page": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.php",
+    "playingTime": "120 minutes",
+    "thumb": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
+    "type": "Hello",
+  },
+  {
+    "brand": "Apple",
+    "color": "Yellow",
+    "id": "ae9b616f81",
+    "img": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.jpg",
+    "page": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.php",
+    "playingTime": "60 minutes",
+    "thumb": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
+    "type": "World",
+  },
+  {
+    "brand": "Sony",
+    "color": "Grey",
+    "id": "ae9b616f8d",
+    "img": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.jpg",
+    "page": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417.php",
+    "playingTime": "090 minutes",
+    "thumb": "http://www.tapedeck.org/400/sony_cdit_ii_90_080417_thumb.jpg",
+    "type": "Chrome",
+  },
+]
+`);
       });
 
       it("should sort the cassettes by brand name", async () => {
         const cassettes = await getAllCassettes();
         expect(cassettes[0].brand).toEqual("Apple");
         expect(cassettes[1].brand).toEqual("Sony");
+      });
+
+      it("should be able to filter by brand name", async () => {
+        const cassettes = await getAllCassettes({ brand: "Apple" });
+
+        expect(cassettes.length).toEqual(1);
+        expect(cassettes[0].brand).toEqual("Apple");
+      });
+      it("should be able to filter by color ", async () => {
+        const cassettes = await getAllCassettes({ color: "Chrome" });
+
+        expect(cassettes.length).toEqual(2);
+        expect(cassettes[0].color).toEqual("Chrome");
+      });
+
+      it("should be able to filter by type ", async () => {
+        const cassettes = await getAllCassettes({ type: "World" });
+
+        expect(cassettes.length).toEqual(1);
+        expect(cassettes[0].type).toEqual("World");
+      });
+      it("should be able to filter by playingTime ", async () => {
+        const cassettes = await getAllCassettes({ playingTime: "60mins" });
+
+        expect(cassettes.length).toEqual(1);
+        expect(cassettes[0].color).toEqual("60 minutes");
+      });
+      it("should be able to combine categories using an AND relationship", async () => {
+        const cassettes = await getAllCassettes({
+          color: "Chrome",
+          brand: "Apple",
+        });
+
+        expect(cassettes.length).toEqual(1);
+        expect(cassettes[0].color).toEqual("Chrome");
+        expect(cassettes[0].brand).toEqual("Apple");
       });
     });
   });
