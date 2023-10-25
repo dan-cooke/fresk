569e81c05e9e677c4a99742b7d47f9542dbeb36d
Daniel Cooke <danielcooke1996@gmail.com>
2023-10-25 17:42:56 +0200

    Switch to vercel because fly was not playinf nice with secrets
    * Its my first time using fly.io with next13 and it doesnt seem to inject my secrets in during my build process

--- lib/api.ts
+++ lib/api.ts
@@ -2,12 +2,14 @@ import { Cassette } from "@/app/types";
 import { FilterOptions, filterCassettes, getFilterOptions } from "./filters";
 
 const { API_KEY, API_URL } = process.env;
+console.log(process.env.API_KEY);
+console.log(process.env);
 
 if (!API_KEY) {
-	throw new Error("No API_KEY found in environment");
+  throw new Error("No API_KEY found in environment");
 }
 if (!API_URL) {
-	throw new Error("No API_URL found in environment");
+  throw new Error("No API_URL found in environment");
 }
 
 /**
@@ -17,98 +19,98 @@ if (!API_URL) {
  */
 
 export const baseFetch = async <TResponse>(
-	endpoint: string,
-	options: RequestInit = {},
+  endpoint: string,
+  options: RequestInit = {},
 ) => {
-	const response = await fetch(`${API_URL}${endpoint}`, {
-		...options,
-		headers: {
-			...options?.headers,
-			"x-api-key": API_KEY,
-			"Content-Type": "application/json",
-		},
-		next: {
-			revalidate: 3600,
-		},
-	});
-	if (!response.ok) {
-		throw new Error(response.statusText);
-	}
-	return response.json() as TResponse;
+  const response = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      "x-api-key": API_KEY,
+      "Content-Type": "application/json",
+    },
+    next: {
+      revalidate: 3600,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response.json() as TResponse;
 };
 
 type CassetteApiData =
-	| { page: string }
-	| { img: string }
-	| { playingTime: string }
-	| { thumb: string }
-	| { type: string }
-	| { color: string }
-	| { brand: string };
+  | { page: string }
+  | { img: string }
+  | { playingTime: string }
+  | { thumb: string }
+  | { type: string }
+  | { color: string }
+  | { brand: string };
 
 export type GetAllCassettesResponse = Record<string, CassetteApiData[]>[];
 
 export type GetAllCassettesFilters = {
-	brand?: string;
-	color?: string;
-	playingTime?: string;
-	type?: string;
+  brand?: string;
+  color?: string;
+  playingTime?: string;
+  type?: string;
 };
 export type GetAllCassettesPage = {
-	page?: number;
-	pageSize?: number;
+  page?: number;
+  pageSize?: number;
 };
 export type GetAllCassettesOptions = {
-	filters?: GetAllCassettesFilters;
-	pagination?: GetAllCassettesPage;
+  filters?: GetAllCassettesFilters;
+  pagination?: GetAllCassettesPage;
 };
 
 export const getAllCassettes = async (
-	options: GetAllCassettesOptions = {},
+  options: GetAllCassettesOptions = {},
 ): Promise<{
-	cassettes: Cassette[];
-	totalResults: number;
-	availableFilters: FilterOptions;
+  cassettes: Cassette[];
+  totalResults: number;
+  availableFilters: FilterOptions;
 }> => {
-	const t = Date.now();
-	// This is the root endpoint of the api
-	const data = await baseFetch<GetAllCassettesResponse>("/");
+  const t = Date.now();
+  // This is the root endpoint of the api
+  const data = await baseFetch<GetAllCassettesResponse>("/");
 
-	const page = options.pagination?.page || 0;
-	const pageSize = options.pagination?.pageSize || 20;
+  const page = options.pagination?.page || 0;
+  const pageSize = options.pagination?.pageSize || 20;
 
-	console.info("API call took", Date.now() - t, "ms");
+  console.info("API call took", Date.now() - t, "ms");
 
-	// this API returns a very strange structure, so lets map it to something more useful
-	const cassettes: Cassette[] = data.map((cassette) => {
-		const [id, properties] = Object.entries(cassette)[0];
-		const cassetteData = properties.reduce((acc, property) => {
-			return { ...acc, ...property };
-		}, {} as Cassette);
-		return {
-			...cassetteData,
-			id,
-		} as Cassette;
-	});
+  // this API returns a very strange structure, so lets map it to something more useful
+  const cassettes: Cassette[] = data.map((cassette) => {
+    const [id, properties] = Object.entries(cassette)[0];
+    const cassetteData = properties.reduce((acc, property) => {
+      return { ...acc, ...property };
+    }, {} as Cassette);
+    return {
+      ...cassetteData,
+      id,
+    } as Cassette;
+  });
 
-	const filteredCassettes = filterCassettes(cassettes, options.filters);
+  const filteredCassettes = filterCassettes(cassettes, options.filters);
 
-	const sortedCassettes = filteredCassettes.sort(
-		(a, b) => a.brand?.localeCompare(b.brand),
-	);
+  const sortedCassettes = filteredCassettes.sort(
+    (a, b) => a.brand?.localeCompare(b.brand),
+  );
 
-	// we have to build the available filters in this function
-	// its not ideal, this function is doing a lot
-	// but ideally this would just all be done on the backend with
-	// some kind of facetted query
-	const availableFilters = getFilterOptions(filteredCassettes);
+  // we have to build the available filters in this function
+  // its not ideal, this function is doing a lot
+  // but ideally this would just all be done on the backend with
+  // some kind of facetted query
+  const availableFilters = getFilterOptions(filteredCassettes);
 
-	return {
-		cassettes: sortedCassettes.slice(
-			page * pageSize,
-			page * pageSize + pageSize,
-		),
-		totalResults: cassettes.length,
-		availableFilters,
-	};
+  return {
+    cassettes: sortedCassettes.slice(
+      page * pageSize,
+      page * pageSize + pageSize,
+    ),
+    totalResults: cassettes.length,
+    availableFilters,
+  };
 };
