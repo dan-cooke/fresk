179b47ded140c75a27ae0f138bcd5db7d44cc46e
Daniel Cooke <danielcooke1996@gmail.com>
2023-10-26 11:11:43 +0200

    Remove link from tags

--- app/components/tag.tsx
+++ app/components/tag.tsx
@@ -1,14 +1,9 @@
-import Link from "next/link";
 import { Button } from "./button";
 
 export type TagProps = {
   children: string;
   hrefBaseSlug?: string;
 };
-export function Tag({ children, hrefBaseSlug = "posts" }: TagProps) {
-  return (
-    <Button>
-      <Link href={`/${hrefBaseSlug}?tag=${children}`}>{children}</Link>
-    </Button>
-  );
+export function Tag({ children }: TagProps) {
+  return <Button>{children}</Button>;
 }
